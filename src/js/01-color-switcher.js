689d11ca10adcfc6aef7bc71ffa4e99ec3a97654
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-09-03 14:32:25 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,37 @@
+/*
+Для генерации случайного цвета используй функцию getRandomHexColor.
+*/
+/*
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// debuged
-function getRandomHexColorS() {
+*/
+function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyElem: document.querySelector('body'),
+  btnStartElem: document.querySelector('button[data-start]'),
+  btnStopElem: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+refs.btnStopElem.disabled = true;
+
+refs.btnStartElem.addEventListener('click', () => {
+  refs.btnStartElem.disabled = true;
+  refs.btnStopElem.disabled = false;
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    //console.log(color);
+    refs.bodyElem.style.background = color;
+  }, 1000);
+});
+
+refs.btnStopElem.addEventListener('click', () => {
+  refs.btnStartElem.disabled = false;
+  refs.btnStopElem.disabled = true;
+  clearTimeout(timerId);
+});
