54bb7d188b6bf6d37af590f9f15d175f75d084c9
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-09-04 15:44:46 +0300

    task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,6 +5,7 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   inputElem: document.querySelector('input#datetime-picker'),
@@ -14,27 +15,61 @@ const refs = {
   minutesElem: document.querySelector('span[data-minutes]'),
   secondsElem: document.querySelector('span[data-seconds]'),
 };
+let goalDate;
+let timerId = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    if (timerId) {
+      Notiflix.Notify.failure('будь ласка, оновіть сторінку :)');
+      return;
+    }
+    if (Date.now() > selectedDates[0]) {
+      Notiflix.Notify.failure('будь ласка, оберіть дату у майбутньому :)');
+      return;
+    }
     console.log(selectedDates[0]);
+    goalDate = selectedDates[0];
+    //console.log(goalDate);
     refs.startElem.disabled = false;
   },
 };
 
-const fp = flatpickr(refs.inputElem, options);
-console.log(fp);
+flatpickr(refs.inputElem, options);
 
 refs.startElem.disabled = true;
 
 console.log(options.defaultDate);
 
 refs.startElem.addEventListener('click', () => {
+  startTimer();
   refs.startElem.disabled = true;
 });
+
+function startTimer() {
+  Notiflix.Notify.success('відлік почався');
+  timerId = setInterval(() => {
+    const difTime = goalDate - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(difTime);
+    refs.daysElem.textContent = addLeadingZero(days);
+    refs.hoursElem.textContent = addLeadingZero(hours);
+    refs.minutesElem.textContent = addLeadingZero(minutes);
+    refs.secondsElem.textContent = addLeadingZero(seconds);
+
+    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+      clearInterval(timerId);
+      refs.startElem.disabled = false;
+      Notiflix.Notify.info('відлік закінчився');
+    }
+  }, 1000);
+}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
