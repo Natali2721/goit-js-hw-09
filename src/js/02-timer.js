2c9893adff79f40b80537bb7b401f2e899ead7cf
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-09-04 14:00:48 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,40 @@
 // commonjs
 //const flatpickr = require('flatpickr');
-
 // es modules are recommended, if available, especially for typescript
 // Описан в документации
-
 import flatpickr from 'flatpickr';
-
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  inputElem: document.querySelector('input#datetime-picker'),
+  startElem: document.querySelector('button[data-start]'),
+  daysElem: document.querySelector('span[data-days]'),
+  hoursElem: document.querySelector('span[data-hours]'),
+  minutesElem: document.querySelector('span[data-minutes]'),
+  secondsElem: document.querySelector('span[data-seconds]'),
+};
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    refs.startElem.disabled = false;
+  },
+};
+
+const fp = flatpickr(refs.inputElem, options);
+console.log(fp);
+
+refs.startElem.disabled = true;
+
+console.log(options.defaultDate);
+
+refs.startElem.addEventListener('click', () => {
+  refs.startElem.disabled = true;
+});
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -28,6 +55,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+//console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
